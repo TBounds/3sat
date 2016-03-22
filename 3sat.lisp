ec096cf71ec13fdd4a24379d5d291aefdecee406
Tyler James Bounds <tyler.bounds@wsu.edu>
2016-03-22 12:47:26 -0700

    Cheesin it

--- 3sat.lisp
+++ 3sat.lisp
@@ -93,12 +93,12 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun simple-hill-climb (clauses state dist unsat)
   (cond 
-    ((null unsat) t)  ; Base case 1: unsat !empty
+    ((null unsat) state)  ; Base case 1: unsat !empty
     ((<= dist 0) nil)  ; Base case 2: dist > 0
   (t 
     (let ((vars (get-all-vars clauses))) ; Get all the vars of clauses and save them.
       (let ((new_state (car (get-better-neighbor clauses state vars (length (unsat-clauses clauses state))))))   ; Find a better neighbor and save the state.
         (let ((new_unsat (unsat-clauses clauses new_state))) ; Find the unsatisfied clauses given the new_state and save them.
           (cond
-            ((> unsat (length new_unsat)) (simple-hill-climb clauses new_state (- dist 1) new_unsat))
-            ((null new_state) t) ) ) ) ) ) ) )
+            ((null new_state) state)
+            ((> unsat (length new_unsat)) (simple-hill-climb clauses new_state (- dist 1) new_unsat))) ) ) ) ) ) )
